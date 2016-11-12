91b57e28d31de5c675b0eb2331e2872cc40885b8
michael febrianto <michaelfebrianto@gmail.com>
2016-11-12 12:51:20 +1100

    master : update rancher-cli location

--- install_rancher_cli.sh
+++ install_rancher_cli.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-wget -qO- https://github.com/rancher/cli/releases/download/v0.2.0/rancher-linux-amd64-v0.2.0.tar.gz | tar xz
-sudo mv ./rancher-v0.2.0/rancher /usr/local/bin/rancher
+wget -q -O rancher https://s3-ap-southeast-2.amazonaws.com/rancherise/rancher-cli/rancher-v0.2.0/rancher
+sudo mv ./rancher /usr/local/bin/rancher
 rancher -version
